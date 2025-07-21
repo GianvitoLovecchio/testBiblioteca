e757bfac39c68bb574aa4603cdbc4a99ff8108e6
Gianvito Lovecchio <gianvito1996@gmail.com>
2025-07-21 21:11:52 +0200

    inizializzazione progetto e installazione tailwindcss

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Test Bibiloteca</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+
+    </head>
+    <body class="bg-[#FDFDFC]">
+        <h1 class="my-6 text-4xl text-black text-center">Benvenuti nella biblioteca</h1>
+    </body>
+</html>
